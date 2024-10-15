b2db603536f41ab12397565f7b4ecb13351c20d8
m23 <maxmaxeltek@gmail.com>
2024-10-15 13:34:09 +0200

    modified:   main.js 	modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   public/index.js 	modified:   public/style.css 	new file:   shutdown-files/WOL-Manager-Shutdown-Client-Installer.sh 	new file:   shutdown-files/shutdown-client.js
        - Added the API requests for shutdown

--- main.js
+++ main.js
@@ -7,28 +7,30 @@ const fetch = require('node-fetch');
 const wol = require('wake_on_lan');
 const bodyParser = require('body-parser');
 const ping = require('ping');
+// const { generateKeyPair } = require("./ssh")
 
 // VARS //
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static('public'));
+
 const DatabasePath = "./servers.json"
 let servers = null
 const cCheckAutoModeInterval = 60 // In Sec
 const cWakingInterval = 30 // In Sec
 let WakingServersArray = []
-const Debugging = false
+const Debugging = true
 
 // Functions //
 // Ping Function
 async function checkServerStatus(ip) {
   try {
-    const result = await ping.promise.probe(ip, { timeout: 5 });
-    return result.alive
+	const result = await ping.promise.probe(ip, { timeout: 5 });
+	return result.alive
 
   } catch (error) {
-    console.warn("Error while pinging ", ip)
-    return false
+	console.warn("Error while pinging ", ip)
+	return false
   }
 }
 
@@ -38,20 +40,31 @@ function sleep(ms) {
 
 
 // API Functions //
+// API to manually send WOL packet
+app.post('/api/shutdown/:index', (req, res) => {
+	const server = servers[req.params.index];
+	if (Debugging) {console.log("DEBUG: API CALL: POST /api/shutdown with data: ", server)}
+	if (!server.allowShutdown) {
+		res.send("Shutdown not allowed for ", server.name);
+	} else {
+
+	}
+});
+
 // API to get servers
 app.get('/api/servers', async (req, res) => {
   if (Debugging) {console.log("DEBUG: API CALL: GET /api/servers")}
   const serversWithoutStatus = servers.map(server => {
-    return { ...server };
+	return { ...server };
   });
   res.json(serversWithoutStatus);
 });
 
 // API to add a new server
 app.post('/api/servers', (req, res) => {
-  const { name, ip, mac, autoMode } = req.body;
-  if (Debugging) {console.log("DEBUG: API CALL: POST /api/servers with data", name, " | ", ip, " | ", mac, " | ", autoMode, " | ", )}
-  servers.push({ name, ip, mac, autoMode });
+  const { name, ip, mac, autoMode, allowShutdown } = req.body;
+  if (Debugging) {console.log("DEBUG: API CALL: POST /api/servers with data: ", name, " | ", ip, " | ", mac, " | ", autoMode, " | ", allowShutdown )}
+  servers.push({ name, ip, mac, autoMode, allowShutdown });
   fs.writeFileSync('./servers.json', JSON.stringify(servers, null, 2));
   res.status(201).send('Server added');
 });
@@ -59,22 +72,22 @@ app.post('/api/servers', (req, res) => {
 // API to manually send WOL packet
 app.post('/api/wake/:index', (req, res) => {
   const server = servers[req.params.index];
-  if (Debugging) {console.log("DEBUG: API CALL: POST /api/wake with data", server)}
+  if (Debugging) {console.log("DEBUG: API CALL: POST /api/wake with data: ", server)}
   wol.wake(server.mac, (err) => {
-    if (err) {
-      if (Debugging) {console.log("DEBUG: API CALL: /api/wake ERROR: ")}
-      console.error(err)
-      res.status(500).send('Failed to send WOL packet');
-    } else {
-      if (Debugging) {console.log("DEBUG: API CALL: /api/wake sent Magic Packet!")}
-      res.send('Wake packet sent');
-    }
+	if (err) {
+	  if (Debugging) {console.log("DEBUG: API CALL: /api/wake ERROR: ")}
+	  console.error(err)
+	  res.status(500).send('Failed to send WOL packet');
+	} else {
+	  if (Debugging) {console.log("DEBUG: API CALL: /api/wake sent Magic Packet!")}
+	  res.send('Wake packet sent');
+	}
   });
 });
 
 // API to toggle auto mode
 app.post('/api/toggle/:index', (req, res) => {
-  if (Debugging) {console.log("DEBUG: API CALL: POST /api/toggle with data", index)}
+  if (Debugging) {console.log("DEBUG: API CALL: POST /api/toggle with data: ", index)}
   servers[req.params.index].autoMode = !servers[req.params.index].autoMode;
   fs.writeFileSync('./servers.json', JSON.stringify(servers, null, 2));
   res.send('Auto mode toggled');
@@ -83,9 +96,9 @@ app.post('/api/toggle/:index', (req, res) => {
 // API to remove a server
 app.delete('/api/servers/:index', (req, res) => {
   const index = req.params.index;
-  if (Debugging) {console.log("DEBUG: API CALL: DELETE /api/servers with data", index)}
+  if (Debugging) {console.log("DEBUG: API CALL: DELETE /api/servers with data: ", index)}
   if (index < 0 || index >= servers.length) {
-    return res.status(400).send('Invalid index');
+	return res.status(400).send('Invalid index');
   }
   servers.splice(index, 1);
   fs.writeFileSync('./servers.json', JSON.stringify(servers, null, 2));
@@ -96,26 +109,26 @@ app.delete('/api/servers/:index', (req, res) => {
 app.get('/api/status/:index?', async (req, res) => {
   const { index } = req.params;
   if (index === undefined) {
-    if (Debugging) {console.log("DEBUG: API CALL: GET /api/status with no data")}
-    // No index provided, return status for all servers
-    const serverStatusPromises = servers.map(async (server) => {
-      const isOnline = await checkServerStatus(server.ip);
-      return { ...server, isOnline };
-    });
+	if (Debugging) {console.log("DEBUG: API CALL: GET /api/status")}
+	// No index provided, return status for all servers
+	const serverStatusPromises = servers.map(async (server) => {
+	  const isOnline = await checkServerStatus(server.ip);
+	  return { ...server, isOnline };
+	});
 
-    const allServersWithStatus = await Promise.all(serverStatusPromises);
-    res.json(allServersWithStatus);
+	const allServersWithStatus = await Promise.all(serverStatusPromises);
+	res.json(allServersWithStatus);
   } else {
-    if (Debugging) {console.log("DEBUG: API CALL: GET /api/status with data", index)}
-    // Index provided, return status for a specific server
-    const server = servers[index];
+	if (Debugging) {console.log("DEBUG: API CALL: GET /api/status with data: ", index)}
+	// Index provided, return status for a specific server
+	const server = servers[index];
 
-    if (!server) {
-      return res.status(404).json({ error: 'Server not found' });
-    }
+	if (!server) {
+	  return res.status(404).json({ error: 'Server not found' });
+	}
 
-    const isOnline = await checkServerStatus(server.ip);
-    res.json({ ...server, isOnline });
+	const isOnline = await checkServerStatus(server.ip);
+	res.json({ ...server, isOnline });
   }
 });
 
@@ -124,22 +137,22 @@ app.get('/api/status/:index?', async (req, res) => {
 // Send WOL packet to wake server
 async function attemptWake(server, retries) {
   while (retries > 0) {
-    resultPing = await ping.promise.probe(server.ip, { timeout: 5 });
+	resultPing = await ping.promise.probe(server.ip, { timeout: 5 });
 
-    if (resultPing.alive) {
-      break
-    } else {
-      wol.wake(server.mac, (err) => {
-        if (err) {
-          console.warn(`Failed to wake ${server.name}:`, err);
-        } else {
-          console.log("Wake packet sent to Server ", server.name, " (", server.ip, ") Trys left: ", retries);
-        }
-      });
-    }
-    
-    await sleep(cWakingInterval);
-    retries -= 1;
+	if (resultPing.alive) {
+	  break
+	} else {
+	  wol.wake(server.mac, (err) => {
+		if (err) {
+		  console.warn(`Failed to wake ${server.name}:`, err);
+		} else {
+		  console.log("Wake packet sent to Server ", server.name, " (", server.ip, ") Trys left: ", retries);
+		}
+	  });
+	}
+	
+	await sleep(cWakingInterval);
+	retries -= 1;
   }
   return 0;
 }
@@ -147,47 +160,47 @@ async function attemptWake(server, retries) {
 // Handle wake attempts
 async function checkAndWake(server, index) {
   try {
-    let resultPing = await ping.promise.probe(server.ip, { timeout: 5 });
-    if (Debugging) {console.log("DEBUG: Checking if ", server.name, " (", server.ip, ") is alive: ", resultPing.alive)}
-    if (!resultPing.alive) {
-      console.log("Server ", server.name, " (",server.ip,") is down. Attempting to wake up...")
-      const resultWake = await attemptWake(server, 3)
-      resultPing = await ping.promise.probe(server.ip, { timeout: 5 });
+	let resultPing = await ping.promise.probe(server.ip, { timeout: 5 });
+	if (Debugging) {console.log("DEBUG: Checking if ", server.name, " (", server.ip, ") is alive: ", resultPing.alive)}
+	if (!resultPing.alive) {
+	  console.log("Server ", server.name, " (",server.ip,") is down. Attempting to wake up...")
+	  const resultWake = await attemptWake(server, 3)
+	  resultPing = await ping.promise.probe(server.ip, { timeout: 5 });
 
-      if (resultWake == 0) {
-        if (!resultPing.alive) {
-          console.log("Server ",server.name, " (",server.ip,") is down! Failed to Wake!")
-        } else {
-          console.log("Server ",server.name, " (",server.ip,") is up!")
-        }
-        // Remove server.ip from Array
-        let indexArray = WakingServersArray.indexOf(server.ip);
-        if (indexArray !== -1) {
-          WakingServersArray.splice(indexArray, 1);
-        }
-      }
-    } else {
-      // If server.ip alive, remove from Array
-      let indexArray = WakingServersArray.indexOf(server.ip);
-      if (indexArray !== -1) {
-        WakingServersArray.splice(indexArray, 1);
-      }
-    }
+	  if (resultWake == 0) {
+		if (!resultPing.alive) {
+		  console.log("Server ",server.name, " (",server.ip,") is down! Failed to Wake!")
+		} else {
+		  console.log("Server ",server.name, " (",server.ip,") is up!")
+		}
+		// Remove server.ip from Array
+		let indexArray = WakingServersArray.indexOf(server.ip);
+		if (indexArray !== -1) {
+		  WakingServersArray.splice(indexArray, 1);
+		}
+	  }
+	} else {
+	  // If server.ip alive, remove from Array
+	  let indexArray = WakingServersArray.indexOf(server.ip);
+	  if (indexArray !== -1) {
+		WakingServersArray.splice(indexArray, 1);
+	  }
+	}
   } catch (error) {
-    console.log("Server ",server.name, " (", server.ip, ") is down! Error!")
-    console.error(err)
+	console.log("Server ",server.name, " (", server.ip, ") is down! Error!")
+	console.error(err)
   }
 }
 
 // INIT //
 console.clear();
 console.log(`
-    ██╗      ██████╗ ██╗   ██╗    ███╗   ███╗██████╗ ██████╗
-    ╚██╗     ██╔══██╗╚██╗ ██╔╝    ████╗ ████║╚════██╗╚════██╗
-     ╚██╗    ██████╔╝ ╚████╔╝     ██╔████╔██║ █████╔╝ █████╔╝
-     ██╔╝    ██╔══██╗  ╚██╔╝      ██║╚██╔╝██║██╔═══╝  ╚═══██╗
-    ██╔╝     ██████╔╝   ██║       ██║ ╚═╝ ██║███████╗██████╔╝
-    ╚═╝      ╚═════╝    ╚═╝       ╚═╝     ╚═╝╚══════╝╚═════╝                                                
+	██╗      ██████╗ ██╗   ██╗    ███╗   ███╗██████╗ ██████╗
+	╚██╗     ██╔══██╗╚██╗ ██╔╝    ████╗ ████║╚════██╗╚════██╗
+	 ╚██╗    ██████╔╝ ╚████╔╝     ██╔████╔██║ █████╔╝ █████╔╝
+	 ██╔╝    ██╔══██╗  ╚██╔╝      ██║╚██╔╝██║██╔═══╝  ╚═══██╗
+	██╔╝     ██████╔╝   ██║       ██║ ╚═╝ ██║███████╗██████╔╝
+	╚═╝      ╚═════╝    ╚═╝       ╚═╝     ╚═╝╚══════╝╚═════╝                                                
 `);
 console.log("WOL-Manager V1.0")
 if (Debugging) {console.log("DEBUG: Debugging is Enabled!")}
@@ -204,13 +217,13 @@ servers = require('./servers.json');
 // AutoMode Loop
 setInterval(() => {
   servers.forEach((server, index) => {
-    if (server.autoMode) {
-      // If server.ip is not found in array
-      if (WakingServersArray.indexOf(server.ip) == -1) {
-        WakingServersArray.push(server.ip)
-        checkAndWake(server, index);
-      }
-    }
+	if (server.autoMode) {
+	  // If server.ip is not found in array
+	  if (WakingServersArray.indexOf(server.ip) == -1) {
+		WakingServersArray.push(server.ip)
+		checkAndWake(server, index);
+	  }
+	}
   });
 }, cCheckAutoModeInterval * 1000);
 
--- public/index.js
+++ public/index.js
@@ -12,37 +12,38 @@ async function fetchServers() {
   serverList.innerHTML = '';
 
   servers.forEach((server, index) => {
-    const serverDiv = document.createElement('div');
-    const dotStyle = `width: 10px; height: 10px; border-radius: 50%; background-color: #9c0d0d; display: inline-block; margin-right: 10px; vertical-align: middle;`; // default dot color (offline)
+	const serverDiv = document.createElement('div');
+	const dotStyle = `width: 10px; height: 10px; border-radius: 50%; background-color: #9c0d0d; display: inline-block; margin-right: 10px; vertical-align: middle;`; // default dot color (offline)
 
-    serverDiv.innerHTML = `
-      <h3>
-        <span id="status-dot-${index}" style="${dotStyle}"></span>${server.name} (${server.ip})
-      </h3>
-      <label>
-      <input type="checkbox" ${server.autoMode ? 'checked' : ''} onchange="toggleAutoMode(${index})">
-      Auto Mode
-      </label>
-      <button class="wake_btn" onclick="wakeServer(${index})">Wake</button>
-      <button class="remove_btn" onclick="removeServer(${index})">Remove</button>
-    `;
-    serverList.appendChild(serverDiv);
+	serverDiv.innerHTML = `
+	  <h3>
+		<span id="status-dot-${index}" style="${dotStyle}"></span>${server.name} (${server.ip})
+	  </h3>
+	  <label>
+	  <input type="checkbox" ${server.autoMode ? 'checked' : ''} onchange="toggleAutoMode(${index})">
+	  Auto Mode
+	  </label>
+	  <button class="wake_btn button" onclick="wakeServer(${index})">Wake</button>
+	  ${server.allowShutdown ? `<button class="shutdown_btn button" onclick="shutdownServer(${index})">Shutdown</button>` : ''}
+	  <button class="remove_btn button" onclick="removeServer(${index})">Remove</button>
+	`;
+	serverList.appendChild(serverDiv);
   });
 }
 
 // Get and update Statuses of servers
 async function updateServerStatuses() {
   try {
-    const response = await fetch('/api/status');
-    const servers = await response.json();
+	const response = await fetch('/api/status');
+	const servers = await response.json();
 
-    servers.forEach((server, index) => {
-      const dot = document.getElementById(`status-dot-${index}`);
-      const dotColor = server.isOnline ? '#1db954' : '#9c0d0d'; // online is green, offline is red
-      dot.style.backgroundColor = dotColor;
-    });
+	servers.forEach((server, index) => {
+	  const dot = document.getElementById(`status-dot-${index}`);
+	  const dotColor = server.isOnline ? '#1db954' : '#9c0d0d'; // online is green, offline is red
+	  dot.style.backgroundColor = dotColor;
+	});
   } catch (error) {
-    console.error('Failed to fetch server statuses:', error);
+	console.error('Failed to fetch server statuses:', error);
   }
 }
 
@@ -54,84 +55,100 @@ function showNotification(message, type) {
   notification.innerText = message;
   notification.style.display = 'block';
   setTimeout(() => {
-    notification.style.opacity = '0';
-    setTimeout(() => {
-      notification.style.display = 'none';
-      notification.style.opacity = '1'; 
-    }, 500); 
+	notification.style.opacity = '0';
+	setTimeout(() => {
+	  notification.style.display = 'none';
+	  notification.style.opacity = '1'; 
+	}, 500); 
   }, 3000); 
 }
 
 // API FUNCTIONS //
+
+// shutdown API Call
+async function shutdownServer(index) {
+	try {
+	  const response = await fetch(`/api/shutdown/${index}`, { method: 'POST' });
+	  if (!response.ok) {
+		showNotification('Failed to send shutdown request.', 'error');
+	  }
+	  showNotification('Shutdown request successfully send.', 'success');
+	} catch (error) {
+	  showNotification('Failed to send shutdown request.', 'error');
+	}
+}
+
 // WakeServer API Call
 async function wakeServer(index) {
   try {
-    const response = await fetch(`/api/wake/${index}`, { method: 'POST' });
-    if (!response.ok) {
-      showNotification('Failed to send wake packet.', 'error');
-    }
-    showNotification('Wake packet sent successfully.', 'success');
+	const response = await fetch(`/api/wake/${index}`, { method: 'POST' });
+	if (!response.ok) {
+	  showNotification('Failed to send wake packet.', 'error');
+	}
+	showNotification('Wake packet sent successfully.', 'success');
   } catch (error) {
-    showNotification('Failed to send wake packet.', 'error');
+	showNotification('Failed to send wake packet.', 'error');
   }
 }
 
 // Toggle Automode API Call
 async function toggleAutoMode(index) {
   try {
-    const response = await fetch(`/api/toggle/${index}`, { method: 'POST' });
-    if (!response.ok) {
-      showNotification('Failed to toggle Automode.', 'error');
-    }
-    showNotification('Changed Automode successfully.', 'success');
+	const response = await fetch(`/api/toggle/${index}`, { method: 'POST' });
+	if (!response.ok) {
+	  showNotification('Failed to toggle Automode.', 'error');
+	}
+	showNotification('Changed Automode successfully.', 'success');
 
-    fetchServers();
+	fetchServers();
   } catch (error) {
-    showNotification('Failed to toggle Automode.', 'error');
+	showNotification('Failed to toggle Automode.', 'error');
   }
 }
 
 // Remove server API Call
 async function removeServer(index) {
   try {
-    const response = await fetch(`/api/servers/${index}`, { method: 'DELETE' });
-    if (!response.ok) {
-      showNotification('Failed to remove server.', 'error');
-    }
+	const response = await fetch(`/api/servers/${index}`, { method: 'DELETE' });
+	if (!response.ok) {
+	  showNotification('Failed to remove server.', 'error');
+	}
 
-    fetchServers();
-    showNotification('Server removed successfully.', 'success');
+	fetchServers();
+	showNotification('Server removed successfully.', 'success');
   } catch (error) {
-    showNotification('Failed to remove server.', 'error');
+	showNotification('Failed to remove server.', 'error');
   }
 }
 
 // Add New server Handler and API Call
 document.getElementById('new-server-form').addEventListener('submit', async (e) => {
   try {
-    e.preventDefault();
-    const name = document.getElementById('name').value;
-    const ip = document.getElementById('ip').value;
-    const mac = document.getElementById('mac').value;
-    const autoMode = document.getElementById('autoMode').checked;
+	e.preventDefault();
+	const name = document.getElementById('name').value;
+	const ip = document.getElementById('ip').value;
+	const mac = document.getElementById('mac').value;
+	const autoMode = document.getElementById('autoMode').checked;
+	const allowShutdown = document.getElementById("allowShutdown").checked;
 
-    await fetch('/api/servers', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ name, ip, mac, autoMode })
-    });
+	await fetch('/api/servers', {
+	  method: 'POST',
+	  headers: { 'Content-Type': 'application/json' },
+	  body: JSON.stringify({ name, ip, mac, autoMode, allowShutdown })
+	});
 
-    // Clear input fields after adding the server
-    document.getElementById('name').value = '';
-    document.getElementById('ip').value = '';
-    document.getElementById('mac').value = '';
-    document.getElementById('autoMode').checked = false;
+	// Clear input fields after adding the server
+	document.getElementById('name').value = '';
+	document.getElementById('ip').value = '';
+	document.getElementById('mac').value = '';
+	document.getElementById('autoMode').checked = false;
+	document.getElementById("allowShutdown").checked = false
 
-    fetchServers();
-    updateServerStatuses();
-    showNotification('Server added successfully.', 'success');
+	fetchServers();
+	updateServerStatuses();
+	showNotification('Server added successfully.', 'success');
   } catch (error) {
-    showNotification('Failed to remove server.', 'error');
+	showNotification('Failed to remove server.', 'error');
   }
 });
 
